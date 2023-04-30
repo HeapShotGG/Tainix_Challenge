4ec5458dd575017dcef6fcb1cab0689ca350abb1
HeapShotGG <heapshotgg@rootix.fr>
2023-04-30 22:23:32 +0200

    Challenge resolution code (final version)

---
+++ tainix_ch3.py
@@ -0,0 +1,20 @@
+#######################################################################################################
+# TAINIX CHALLENGE : https://tainix.fr/challenge/Boucle-for
+# CHALLENGE LV STARTER - "Boucle for"
+# ENONCE : 
+# Tu dois retourner la somme des valeurs entières de 1 jusque STOP..
+#######################################################################################################
+# @Author: HeapshotGG
+# Date : 30 Avril 2023
+
+
+STOP=60 
+
+# Pour calculer la sommes on initialise une variable à 0
+result = 0
+
+# on parcours la liste jusqu'à atteindre la valeur de STOP.
+for i in range(STOP):
+    # On additione la valeur de i à result
+    result = result + i
+    print(result)
